14d2461f9a10d28ea8f352e6094e9de2ce44a23c
Dan B <dbraunby@hotmail.com>
2021-04-25 14:28:05 -0600

    Initial directory set up and server connection

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP IF EXISTS tech_blog_db;
+CREATE DATABASE tech_blog_db;
+
+SHOW DATABASES;
---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Batman 6561800 9856555 poisonIvy69',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
